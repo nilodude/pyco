25e4d5f30fdf123a029c11c6de85cbe03d58e919
erporati <alphone@protonmail.com>
2024-02-14 19:14:54 +0100

    software OK, bad connections

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ print(i2c.scan())
 
 mcp = MCP23017(i2c, 0x20)
 
-mcp.mode = 0x0000
+mcp.porta.mode = 0x00
+mcp.portb.mode = 0x00
+
+# mcp.gpio = 0x0f00
 mcp.portb.gpio = 0b00001111
 
 # cant understand why, portA4 enables display 4 on or off
-mcp.porta.gpio = 0b11111000
+mcp.porta.gpio = 0b10001001
