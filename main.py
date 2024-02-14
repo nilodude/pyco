eceb4cfc5f9e5d355a7b9ed4292b3dfc12e0931f
erporati <alphone@protonmail.com>
2024-02-14 22:24:09 +0100

    reading encoder & sending data to 7 segment

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from mcp23017 import MCP23017
 led = Pin("LED", Pin.OUT)
 tim = Timer()
 
+clk_encoder = Pin(2, Pin.IN, Pin.PULL_UP)
+dt_encoder = Pin(3, Pin.IN, Pin.PULL_UP)
+sw_encoder = Pin(4, Pin.IN, Pin.PULL_UP)
+
+
+currCLK = 0
+counter = 0
+lastCLK = clk_encoder.value()
+lastBtnPres = 0
+pushBtn = False
+
 zero = 	0b11101110
 one = 	0b00101000
 two = 	0b11011010
@@ -19,23 +30,44 @@ number = [zero, one, two, three,four,five, six, seven, eight,nine]
 arrlen = len(number)
 pos = 0
 
+def readEncoderValue():
+    global clk_encoder
+    global dt_encoder
+    global pushBtn
+    global currCLK
+    global lastCLK
+    global counter
+    
+    currCLK=clk_encoder.value()
+    dt = dt_encoder.value()
+    
+    if(currCLK != lastCLK and currCLK == 1):
+        if(dt != currCLK):
+            counter -= 1
+            print(counter)
+        else:
+            counter += 1
+            print(counter)
+    
+    lastCLK = clk_encoder.value()
+    
+    counter = 0 if counter == 10 else counter
+    counter = 9 if counter == -1 else counter
+    
+    return counter
+
+
 def tick(timer):
     global led
-    global mcp
-    global pos
-    global arrlen
-    
     led.toggle()
-    
-    pos = 0 if pos == arrlen-1 else pos + 1 
-    mcp.porta.gpio = ~number[pos]
-    mcp.portb.gpio ^= 0b00010000
-    mcp.portb.gpio ^= pos 
 
 tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
 
 i2c = I2C(0,scl=Pin(9), sda=Pin(8))
-print(i2c.scan())
+
+addresses = i2c.scan()
+print('i2c device on address:')
+print(hex(addresses[0]) if len(addresses) > 0 else 'no addresses found')
 
 mcp = MCP23017(i2c, 0x20)
 
@@ -45,3 +77,13 @@ mcp.portb.mode = 0x00
 mcp.gpio = 0x0f00
 mcp.portb.gpio = 0b00011111
 
+
+while(True):
+    encoderValue = readEncoderValue()
+    
+    mcp.porta.gpio = ~number[encoderValue]
+    
+    if(sw_encoder.value() == 0):
+        print('pulsao')
+    
+    
