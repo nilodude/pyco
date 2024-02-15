c8276ad25301cb31a4d9728e6e918f6d970ce07e
erporati <alphone@protonmail.com>
2024-02-15 17:07:31 +0100

    finetuning sleep between displays & ADC i2c1 OK

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ from machine import Pin, Timer, I2C
 from mcp23017 import MCP23017
 import time
 
-led = Pin("LED", Pin.OUT)
-tim = Timer()
-
-clk_encoder = Pin(2, Pin.IN, Pin.PULL_UP)
-dt_encoder = Pin(3, Pin.IN, Pin.PULL_UP)
-sw_encoder = Pin(4, Pin.IN, Pin.PULL_UP)
-
-currCLK = 0
-counter = 0
-lastCLK = clk_encoder.value()
-lastBtnPres = 0
-pushBtn = False
-
 zero = 	0b11101110
 one = 	0b00101000
 two = 	0b11011010
@@ -28,7 +15,35 @@ nine = 	0b00111110
 
 number = [zero, one, two, three,four,five, six, seven, eight,nine]
 displays = [0b00000001,0b00000010,0b00000100,0b00001000]
+led = Pin("LED", Pin.OUT)
+tim = Timer()
 
+clk_encoder = Pin(2, Pin.IN, Pin.PULL_UP)
+dt_encoder = Pin(3, Pin.IN, Pin.PULL_UP)
+sw_encoder = Pin(4, Pin.IN, Pin.PULL_UP)
+currCLK = 0
+counter = 0
+lastCLK = clk_encoder.value()
+lastBtnPres = 0
+pushBtn = False
+
+
+i2c1 = I2C(1,scl=Pin(15), sda=Pin(14))
+addresses = i2c1.scan()
+print('i2c1 device on address:')
+print(hex(addresses[0]) if len(addresses) > 0 else 'no addresses found')
+
+i2c0 = I2C(0,scl=Pin(9), sda=Pin(8))
+addresses = i2c0.scan()
+print('i2c0 device on address:')
+print(hex(addresses[0]) if len(addresses) > 0 else 'no addresses found')
+
+mcp = MCP23017(i2c0, 0x20)
+
+mcp.porta.mode = 0x00
+mcp.portb.mode = 0x00
+mcp.gpio = 0x0f00
+mcp.portb.gpio = 0b00001111
 
 def readEncoderValue():
     global clk_encoder
@@ -66,78 +81,74 @@ def selectNumber(n):
     
 def tick(timer):
     global led
+    global mcp
+    mcp.portb.gpio ^= 0b00010000
     led.toggle()
 
+def sleep(t=0.00095):
+    time.sleep(t)
+
 def number2display(n):
-    
     s = str(n)
-    
     digits = len(s)
     mcp.porta.gpio = 0xff
+    
     if(digits == 1):
-        
         selectDisplay(4)
         selectNumber(int(s))
+        sleep()
         mcp.porta.gpio = 0xff
     elif(digits == 2):
         selectDisplay(3)
         selectNumber(int(s[0]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(4)
         selectNumber(int(s[1]))
+        sleep()
+        mcp.porta.gpio = 0xff
     elif(digits == 3):
         selectDisplay(2)
         selectNumber(int(s[0]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(3)
         selectNumber(int(s[1]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(4)
         selectNumber(int(s[2]))
+        sleep()
+        mcp.porta.gpio = 0xff
     elif(digits == 4):
         selectDisplay(1)
         selectNumber(int(s[0]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(2)
         selectNumber(int(s[1]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(3)
         selectNumber(int(s[2]))
+        sleep()
         mcp.porta.gpio = 0xff
         selectDisplay(4)
         selectNumber(int(s[3]))
-    
-    
-#     for digit in range(digits):
-#         display = 4 - digit
-# #         print('display:', display, 'digit:', s[digits-digit-1])
-#         selectDisplay(display)
-#         mcp.porta.gpio = ~number[int(s[digits-digit-1])]
-#         time.sleep(0.005)
+        sleep()
+        mcp.porta.gpio = 0xff
 
 
 tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
 
-i2c = I2C(0,scl=Pin(9), sda=Pin(8))
-
-addresses = i2c.scan()
-print('i2c device on address:')
-print(hex(addresses[0]) if len(addresses) > 0 else 'no addresses found')
-
-mcp = MCP23017(i2c, 0x20)
-
-mcp.porta.mode = 0x00
-mcp.portb.mode = 0x00
-mcp.gpio = 0x0f00
-mcp.portb.gpio = 0b00011111
 
 while(True):
     encoderValue = readEncoderValue()
     
     number2display(encoderValue)
+    
     mcp.gpio = 0x0fff
     
     if(sw_encoder.value() == 0):
         print('pulsando')
-    
     
